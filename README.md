42a2cc94b7ee79ccd2e7580fbf5288aea09c971d
Thomas <tzhang920314@gmail.com>
2024-11-30 14:57:12 -0500

    chore: new readme for basic guides

---
+++ README.md
@@ -0,0 +1,25 @@
+## Install the cli
+
+```bash
+npm link
+```
+
+## Usage
+
+### Clone the template repository
+
+```bash
+sa-cli init -d <target-dir>
+```
+
+### Create a grid selection component
+
+```bash
+sa-cli create -X 16 -Y 8 -d <target-dir>/<components-dir>
+```
+
+Or just create the component in `./src/components` by default:
+
+```bash
+sa-cli create -X 16 -Y 8
+```
--- bin/index.js
+++ bin/index.js
@@ -7,19 +7,3 @@ import './create.js';
 import { program } from './program.js';
 
 program.parse(process.argv);
-
-
-
-
-// import { program } from './program.js'
-// import './install.js'
-// import './init.js'
-// import './fetch.js'
-// import './sync.js'
-// import './clean.js'
-// import './convert.js'
-// import './create.js'
-// import './link-packages.js'
-
-// const args = process.argv
-// program.parse(args)
